97ee51e7f680c3a5e1f085c8079a4fb6fa0297e6
ThangVoChien <haivo5229@gmail.com>
2025-05-14 16:42:20 +0700

    .

--- demo/demo/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import Hello from "./Hello";
-import React from "react";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Hello/>
-    </React.StrictMode>
-);
