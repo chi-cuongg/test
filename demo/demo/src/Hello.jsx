ed4a3d22077034f301cccc10c846a54113f0fa7f
ThangVoChien <haivo5229@gmail.com>
2025-05-14 13:19:33 +0700

    check hiiiiiiiiii

---
+++ demo/demo/src/Hello.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Hello extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>Hello World</p>
+            </div>
+        );
+    }
+}
+export default Hello
---
+++ demo/demo/src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import Hello from "./Hello";
+import React from "react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Hello/>
+    </React.StrictMode>
+);
